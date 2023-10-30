3e1040da4773cd3a32e4c950503238527fdeef3e
MAHMOUDOMARIBNELKHATAB <momar351@yahoo.com>
2023-10-30 21:36:16 +0200

    Online Shopping Use Api Angular

---
+++ src/app/interface/cards.ts
@@ -0,0 +1,12 @@
+export interface Cards {
+    "id": number,
+    "age": number,
+    "gender" ?: string,
+    "email": string,
+    "phone": string,
+    "username":string,
+    "password": string,
+    "birthDate": string,
+    "image": string,
+    "is_verified": boolean,
+}
